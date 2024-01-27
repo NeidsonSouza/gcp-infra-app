6aa0218d21e2caa3ee168005bea4bb11a3f12de6
Neidson Souza <neidson.ds.souza@gmail.com>
2024-01-27 16:37:20 -0300

    push

---
+++ gcs.tf
@@ -0,0 +1,6 @@
+terraform {
+  backend "gcs" {
+    # bucket = "terraform-tfstate-e8y7"
+    # prefix = "terraform/state/devel"
+  }
+}
--- staging/main..tf
+++
@@ -1,4 +0,0 @@
-resource "google_project" "create" {
-  name       = "fullstack-${var.gcp_env}"
-  project_id = "fullstack-${var.gcp_env}"
-}
