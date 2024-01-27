2a8cee1ed6535a73fd7389f2769354552fcffcf8
Neidson Souza <neidson.ds.souza@gmail.com>
2024-01-27 18:27:08 -0300

    add billing

--- main..tf
+++ main..tf
@@ -1,9 +1,21 @@
 locals {
-  project_id = "fullstack-${var.gcp_env}"
+  project_id = "fullstack-neidson-${var.gcp_env}"
+}
+
+data "google_billing_account" "acct" {
+  display_name = "My Billing Account"
+  open         = true
 }
 
 resource "google_project" "create" {
-  name       = local.project_id
-  project_id = local.project_id
-  org_id     = "776289498881"
+  name            = local.project_id
+  project_id      = local.project_id
+  org_id          = "776289498881"
+  billing_account = data.google_billing_account.acct.id
+}
+
+resource "google_project_service" "art" {
+  project    = local.project_id
+  service    = "artifactregistry.googleapis.com"
+  depends_on = [google_project.create]
 }
