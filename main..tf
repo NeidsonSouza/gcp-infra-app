33cb7b21c4dc9ca4d3c57d5088d0972cca2a0908
Neidson Souza <neidson.ds.souza@gmail.com>
2024-01-27 17:01:24 -0300

    push

--- main..tf
+++ main..tf
@@ -2,6 +2,12 @@ locals {
   project_id = "fullstack-${var.gcp_env}"
 }
 
+resource "google_project_service" "project" {
+  project    = "core-1s4u"
+  service    = "cloudresourcemanager.googleapis.com"
+  depends_on = [google_project.create]
+}
+
 resource "google_project" "create" {
   name       = local.project_id
   project_id = local.project_id
