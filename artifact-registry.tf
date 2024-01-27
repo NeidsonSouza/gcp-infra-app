018281b98bfa041720e32c4a02dd3e1e2a5d4485
Neidson Souza <neidson.ds.souza@gmail.com>
2024-01-27 18:07:54 -0300

    create project

--- artifact-registry.tf
+++ artifact-registry.tf
@@ -3,5 +3,6 @@ resource "google_artifact_registry_repository" "node-repo" {
   repository_id = "node-repo"
   description   = "Docker repository"
   format        = "DOCKER"
+  project       = local.project_id
   depends_on    = [google_project.create]
 }
