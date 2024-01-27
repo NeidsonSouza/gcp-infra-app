39477dec39366c9643b78c5dd66cf31c6ea0cf8f
Neidson Souza <neidson.ds.souza@gmail.com>
2024-01-27 13:10:54 -0300

    create artifact registry

---
+++ devel/artifact-registry.tf
@@ -0,0 +1,6 @@
+resource "google_artifact_registry_repository" "node-repo" {
+  location      = "us-central1"
+  repository_id = "node-repo"
+  description   = "Docker repository"
+  format        = "DOCKER"
+}
